790a84fe5518380cc8b86d068091bea8df063484
phanisaimunipalli <phanisaimunipalli@gmail.com>
2022-10-13 08:13:45 -0700

    CMPE281-Added dummy keys for clientids

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID: "",
-  googleClientSecret: "",
-  faceBookID: "",
-  faceBookSecret: "",
+  googleClientID: "wdsadasdasd",
+  googleClientSecret: "asdsadasdasd",
+  faceBookID: "sadsadasd",
+  faceBookSecret: "asdsadasdads",
   AwsAccessKeyId: "",
   AwsSecretAccessKey: "",
   region: "us-west-1",
@@ -10,5 +10,5 @@ module.exports = {
   AwsS3Url: "https://phani-qrsaver.s3-us-west-1.amazonaws.com/",
   AwsDynamoDbUrl: "https://dynamodb.us-west-1.amazonaws.com",
   MongoURI:
-    "mongodb+srv://phani:phaniadmin@phani-mongo-cluster.dhqlpkq.mongodb.net/qrsaver",
+    "mongodb+srv://phani:phaniadmin@phani-mongo-cluster.dhqlpkq.mongodb.net/qrsaver?retryWrites=true&w=majority",
 };
